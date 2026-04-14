be11ff02441ed26e522da8923f4ce3b190ba3b61
finalSay9 <chimwazaevan1@gmail.com>
2026-04-14 12:31:11 +0200

    creating a user

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express =  require('express')
+const { PrismaClient } = require(@prisma/client)
 const app = express()
+const prisma = new PrismaClient();
 const PORT = 3000
+
+app.use(express.json());
+
+//create user
+
+app.post('/users', async (req, res) => {
+    const user = await prisma.user.create({
+        data: req.body,
+    });
+    res.json(user);
+
+})
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('hello evan, welcome to the expressjs')
