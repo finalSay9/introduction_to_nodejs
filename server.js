264902775490e14ce006e026601dd9a6a3b3064c
finalSay9 <chimwazaevan1@gmail.com>
2026-04-14 13:03:47 +0200

    prisma is a problem man

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+
 const express =  require('express')
 const { PrismaClient } = require("@prisma/client");
 const app = express()
 const prisma = new PrismaClient();
 const PORT = 3000
+
+console.log("DB:", process.env.DATABASE_URL);
 
 app.use(express.json());
 
