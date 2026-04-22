f230e0c0529b622fb5f53166e0a4180daed3822a
finalSay9 <chimwazaevan1@gmail.com>
2026-04-22 15:25:39 +0200

    registering the routes

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
-// Swagger config
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "My API",
-      version: "1.0.0",
-      description: "Simple Express API with Swagger",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
+// Simple Swagger config
+const swaggerSpec = {
+  openapi: "3.0.0",
+  info: {
+    title: "My API",
+    version: "1.0.0",
+    description: "Simple Express API with Swagger",
   },
-  apis: ["./routes/*.js"], // where your route docs live
+  servers: [
+    {
+      url: "http://localhost:3006",
+    },
+  ],
+  paths: {}, // Empty for now
 };
-
-const swaggerSpec = swaggerJsdoc(options);
 
 // Swagger route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-  
-  console.log("Docs at http://localhost:3000/api-docs");
+// Test route
+app.get("/", (req, res) => {
+  res.send("Server is working!");
+});
+
+app.listen(3006, () => {
+  console.log("Server running on http://localhost:3006");
+  console.log("Docs at http://localhost:3006/api-docs");
 });
